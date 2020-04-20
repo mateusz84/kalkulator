70ed98f253aaa2e53b73aa88d18d0219d9341b6f
Mateusz <mateusz.aleksandrowicz@gmail.com>
2020-04-20 13:36:02 +0200

    first commit

---
+++ src/petelka.java
@@ -0,0 +1,13 @@
+public class petelka {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println("przed wykonaniem petli");
+        while (x<4) {
+            System.out.println("wewnatrz petli");
+            System.out.println("wartość x = " + x);
+            x = x + 1;
+        }
+        System.out.println("i juz po petli)");
+        }
+    }
+
